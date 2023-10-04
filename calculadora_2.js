dedba41804e536c79639aecb516ff1fa293fa84e
Alejo D'Aloia <147001757+AlejoDaloia@users.noreply.github.com>
2023-10-04 18:30:58 -0300

    Add files via upload
    Proyecto calculadora 
    materia: Programación I
    Instituto Leibnitz: Carrera de Analista en Sistemas

---
+++ calculadora_2.js
@@ -0,0 +1,51 @@
+let valorDisplay= "";
+let result= "";
+let deshacer="";
+let incluye= false;
+let memoria= "";
+
+function sumarDisplay(value){
+        valorDisplay+=value;
+        document.getElementById("display").value= valorDisplay;
+        if (valorDisplay==="800815"){
+                alert("tetas!!!!111");
+        }
+}
+
+function parentesis(value){
+        incluye= valorDisplay.includes('(');
+        if (incluye===true){
+                valorDisplay+=')';
+                document.getElementById("display").value= valorDisplay;
+        } else{
+                valorDisplay+=value;
+                document.getElementById("display").value= valorDisplay;
+        }
+}
+function limpiarDisplay(){
+        deshacer= valorDisplay;
+        valorDisplay="";
+        document.getElementById("display").value= "";
+}
+
+function del(){
+        deshacer= valorDisplay;
+        valorDisplay= valorDisplay.slice(0, valorDisplay.length - 1);
+        document.getElementById("display").value= valorDisplay;
+}
+
+function mostrar_result(){
+        valorDisplay= eval(valorDisplay).toString();
+        result= valorDisplay
+        document.getElementById("display").value= valorDisplay;
+}
+
+function undo(){
+        valorDisplay= deshacer;
+        document.getElementById("display").value= valorDisplay;
+}
+
+function ans(){
+        document.getElementById("display").value= result;
+        valorDisplay= result;
+}
